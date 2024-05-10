aae38fb81317caf8d6fc208408850dbe03c0963a
janojitukaram <janojitukaram@gmail.com>
2024-05-10 20:22:46 +0530

    pooling checks, updated comment in listner class

--- src/test/java/listeners/ListenersDemo.java
+++ src/test/java/listeners/ListenersDemo.java
@@ -3,7 +3,7 @@ package listeners;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-
+//this comment for polling checks
 public class ListenersDemo implements ITestListener{
 
 	public void onTestStart(ITestResult result) {
